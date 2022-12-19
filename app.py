bad82e992d697a9e28b518c827a49130ea56f6c3
Thomasq99 <t.j.quadt@student.tue.nl>
2022-12-19 21:42:08 +0100

    Finished concept layout

--- app.py
+++ app.py
@@ -26,27 +26,34 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # my GPU is too small to save enough 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 concept_bank_menu = [
-    html.H5('Concept Bank', style={'text-align': 'center', 'margin-bottom': '0px'}),
+    html.H5('Concept Bank', style={'text-align': 'center', 'margin-bottom': '11px'}),
     dbc.Col([
-        dbc.Label('Session directory:', style={'margin-bottom': '0px'}),
-        dbc.Input(value='./ACE_output/ImageNet', id='working_dir', placeholder='working_dir', type='text',
-                  required=True),
-        dbc.Label('Remove Concept:'),
+        html.Div(dbc.Button([dbc.Spinner([html.Div('Automatically extract concepts', id='spinner')])],
+                            id='start_ACE', outline=True, color='primary'),
+                 className="d-grid gap-2"),
         dbc.InputGroup([
             dbc.Button("Remove concept", id='remove_concept_button', outline=True, color='primary', n_clicks=0,
                        disabled=True),
-            dbc.Select(id='remove_concept_select', disabled=True),
-        ]),
+            dbc.Select(id='remove_concept_select', disabled=True)
+        ], style={'margin-top': '15px', 'margin-bottom': '15px'}),
 
-        html.Br(),
+        dcc.Upload(dbc.Button('Upload Zip File of concept', outline=True, color="primary", style={'width': '100%'})),
+
+        html.Div(dbc.Button('Import Concept Bank', id='import_button', outline=True, color='primary',
+                            style={'margin-top': '15px', 'margin-bottom': '15px'}),
+                 className="d-grid gap-2"),
+
+
         dbc.Row([
             dbc.Col(html.Div(dbc.Button('Clear Concept Bank', outline=True, color='primary', id='clear_concept_bank',
                                         n_clicks=0, disabled=True),
                              className="d-grid gap-2"), width=6),
 
-            dbc.Col(dcc.Upload(dbc.Button('Upload Zip File of concept', outline=True, color="primary",
-                                          style={'width': '100%'})), width=6)
+            dbc.Col(html.Div(dbc.Button('Save current Concept Bank', id='save_button', outline=True, color='primary'),
+                             className="d-grid gap-2"), width=6)
         ]),
+
+
         # html.Br(),
         # html.Div(dbc.Button('Load Existing Concept Bank', outline=True, color='primary'), className="d-grid gap-2")
 
@@ -55,11 +62,14 @@ concept_bank_menu = [
 
 # define ace menu
 ace_menu = [
-    html.H5("ACE parameters", style={'text-align': 'center', 'margin-bottom': '0px', 'margin-top': '15px'}),
+    html.H5("Settings", style={'text-align': 'center', 'margin-bottom': '0px', 'margin-top': '15px'}),
+    dbc.Label('Session directory:', style={'margin-bottom': '0px'}),
+    dbc.Input(value='./ACE_output/ImageNet', id='working_dir', placeholder='working_dir', type='text',
+              required=True),
     dbc.Col(
         [
             dbc.Label('Model selection:'),
-            dbc.Input(value='InceptionV3', id='model_selection', type='text'),
+            dbc.Input(value='InceptionV3', id='model_selection', type='text', required=True),
             dbc.Label('Data directory:'),
             dbc.Input(value='./data/ImageNet', id='data_path', placeholder='Path to source_dir or np.array',
                       type='text')
@@ -72,10 +82,7 @@ ace_menu = [
             dbc.Input(id='bottlenecks', placeholder='e.g. mixed8, mixed3',
                       type='text'),
 
-        ], width=6),
-    html.Div(dbc.Button([dbc.Spinner([html.Div('Run ACE', id='spinner')])],
-                        id='start_ACE', outline=True, color='primary'),
-             className="d-grid gap-2")
+        ], width=6)
 ]
 
 app.layout = dbc.Container(children=
@@ -96,12 +103,13 @@ app.layout = dbc.Container(children=
         dbc.Row(
             [
                 dbc.Col(
-                    [
+                    [   # Settings
+                        dbc.Row(ace_menu, style={'height': "50%"}),
+
                         # Concept bank menu definition
-                        dbc.Row(concept_bank_menu, style={'height': "50%"}),
+                        dbc.Row(concept_bank_menu, style={'height': "50%"})
 
                         # ACE Menu definition
-                        dbc.Row(ace_menu, style={'height': "50%"})
                     ],
                     width=4),
 
@@ -169,20 +177,19 @@ def update_concept_bank(b1, b2, b3, model_name, path_to_source, path_to_working_
         for bn in concept_bank_dct:
             for concept in concept_bank_dct[bn]['concept_names']:
                 label = f'{bn}, {concept}'
-                value = f'{bn},{concept}'
-                remove_options.append({'value': value, 'label': label})
+                remove_options.append({'value': label, 'label': label})
 
         bottleneck_options = [{'value': bn, 'label': bn} for bn in bottlenecks]
 
-        return 'Run ACE', concept_bank_dct, False, bottleneck_options, bottlenecks[0], remove_options, False, False, \
-               False, False
+        return 'Automatically extract concepts', concept_bank_dct, False, bottleneck_options, bottlenecks[0], \
+               remove_options, False, False, False, False
 
     elif triggered_id == 'clear_concept_bank':
-        return 'Run ACE', None, False, None, None, None, True, True, True, True
+        return 'Automatically extract concepts', None, False, None, None, None, True, True, True, True
 
     elif triggered_id == 'remove_concept_button':
         #TODO Failsafe if all concepts are removed
-        bottleneck, concept_name = remove_concept_name.split(',')
+        bottleneck, concept_name = remove_concept_name.split(', ')
 
         # load concept bank
         concept_bank_dct = {bn: ConceptBank(concept_bank_dct[bn]) for bn in concept_bank_dct.keys()}
@@ -196,14 +203,13 @@ def update_concept_bank(b1, b2, b3, model_name, path_to_source, path_to_working_
         for bn in concept_bank_dct:
             for concept in concept_bank_dct[bn]['concept_names']:
                 label = f'{bn}, {concept}'
-                value = f'{bn}_{concept}'
-                remove_options.append({'value': value, 'label': label})
+                remove_options.append({'value': label, 'label': label})
 
         bottlenecks = list(concept_bank_dct.keys())
         bottleneck_options = [{'value': bn, 'label': bn} for bn in bottlenecks]
 
-        return 'Run ACE', concept_bank_dct, False, bottleneck_options, bottlenecks[0], remove_options, False, False, \
-               False, False
+        return 'Automatically extract concepts', concept_bank_dct, False, bottleneck_options, bottlenecks[0], \
+               remove_options, False, False, False, False
 
 
 @app.callback([Output('cav_images', 'figure'),
