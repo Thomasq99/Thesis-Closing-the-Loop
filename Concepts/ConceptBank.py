Added tcav_score computation for user_defined concepts and fixed error with adding own concepts

Concepts/ConceptBank.py
@@ -44,7 +44,6 @@ class ConceptBank:
             # TODO implement
             raise ValueError('Not implemented when CAVs are in memory')
         else:
-            # TODO assert to check if exists
             self.concept_names.extend(concepts)
 
     def remove_concept(self, concept_name: str):
@@ -65,7 +64,6 @@ class ConceptBank:
         if (self.tcav_scores is None) or (len(self.tcav_scores)) != (len(self.concept_names)):
             self.compute_tcav_scores(discovery_images=discovery_images)
 
-        # get indices of sorted scores
         sorted_idxs = np.argsort(self.tcav_scores)[::-1]
 
         # sort based on indices
@@ -81,19 +79,15 @@ class ConceptBank:
         @param discovery_images: Dictionary containing an array of discovery images for each class.
             format is {concept_class_name: img_array}
         """
-        # split user defined concepts from automatically extracted concepts
-        concept_names_ACE = [concept for concept in self.concept_names if 'userDefined' not in concept]
-        concept_names_user_defined = [concept for concept in self.concept_names if 'userDefined' in concept]
-
         # For the discovered concepts get the classes for which they were discovered
-        concept_classes = np.array([concept.split('__')[0] for concept in concept_names_ACE])
+        concept_classes = np.array([concept.split('__')[0] for concept in self.concept_names])
 
         # load images if discovery_images is not supplied
         if discovery_images is None:
             discovery_images = {}
-            for concept in set(concept_classes):  # for each concept load the class images
-                image_dir = os.path.join(self.working_dir, 'concepts', concept, 'images')
-                discovery_images[concept] = load_images_from_files([os.path.join(image_dir, file) for file in
+            for class_ in set(concept_classes):  # for each concept load the class images
+                image_dir = os.path.join(self.working_dir, 'concepts', class_, 'images')
+                discovery_images[class_] = load_images_from_files([os.path.join(image_dir, file) for file in
                                                                     os.listdir(image_dir)], do_shuffle=True,
                                                                    max_imgs=40)
 
@@ -103,14 +97,12 @@ class ConceptBank:
 
         model = self.load_model()
 
-        # For each concept automatically extracted using ACE compute the TCAV scores.
+        # For each concept compute the TCAV scores.
         tcav_scores = []
-        concepts_ACE = [self.concepts[i] for i in range(len(self.concepts)) if 'userDefined'
-                        not in self.concept_names[i]]
         for concept_class in set(concept_classes):
             # get gradients of images w.r.t. bottleneck layer
             class_id = self.class_id_dct[concept_class]
-            concepts_of_concept_class = np.array(concepts_ACE)[concept_classes == concept_class]
+            concepts_of_concept_class = np.array(self.concepts)[concept_classes == concept_class]
             discovery_images_of_class = discovery_images[concept_class]
             gradients = get_gradients_of_images(discovery_images_of_class, get_grad_model(model, self.bottleneck),
                                                 class_id)
@@ -118,7 +110,6 @@ class ConceptBank:
             # compute tcav scores
             tcav_scores.extend([concept.compute_tcav_score(gradients) for concept in concepts_of_concept_class])
 
-        tcav_scores.extend([float('-inf') for concept in concept_names_user_defined])  # TODO get TCAV for user defined
         self.tcav_scores = tcav_scores
 
         if not self.in_memory:

Dash_helper.py
@@ -254,6 +254,7 @@ def read_image(upload_contents: str, filename: str, shape: Tuple = (299, 299)) -
         file.write_bytes(decoded)
         img = skimage.io.imread(file)
         img = np.array(Image.fromarray(img).convert('RGB').resize(shape))
+        img = np.float32(img) / 255.0
         return img
 
 
@@ -271,7 +272,7 @@ def get_random_activation(session_dir: str, bottleneck: str) -> List:
 
 
 def create_new_concept(list_of_contents: List, filenames: List, session_dir: str, bottleneck: str,
-                       model: tf.keras.models.Model, mode: str = 'max'):
+                       model: tf.keras.models.Model, target_class, mode: str = 'max'):
     """ Creates a new CAV representing a concept from a list of images. Creates a new CAV trained on each random
         counterpart. The user can either take the maximum CAV accuracy or take the average of all CAVs. The found CAV
         will be stored, so it can be loaded later.
@@ -281,14 +282,16 @@ def create_new_concept(list_of_contents: List, filenames: List, session_dir: str
     @param session_dir: Directory of the session the current user is in.
     @param bottleneck: Name of the bottleneck layer of the model for which the concept will be created.
     @param model: Tensorflow model for which the concept will be found.
+    @param target_class: Name of the target_class.
     @param mode: Mode of averaging the concepts. Can be 'max' or 'average'.
         'max': will take the concept with maximum CAV accuracy.
         'average': will average the learned CAVs out.
     @return: concept_name: The name of the learned concept.
     """
+
     # read in images
     images = np.stack([read_image(content, filename) for content, filename in zip(list_of_contents, filenames)], axis=0)
-    concept_name = f'userDefined_{filenames[0].split("_")[0]}'
+    concept_name = f'{target_class}__userDefined_{filenames[0].split("_")[0]}'
 
     concept_dir = os.path.join(session_dir, 'concepts', concept_name)
 
@@ -326,7 +329,8 @@ def create_new_concept(list_of_contents: List, filenames: List, session_dir: str
         cav_avg.save_cav(os.path.join(session_dir, 'cavs'))
 
     # save images used to train the CAV
-    save_images(os.path.join(concept_dir, 'images'), list(images))
+    images = (images*255).astype(np.uint8)
+    save_images(os.path.join(concept_dir, 'images'), images)
 
     return concept_name
 

test.py
@@ -39,20 +39,71 @@
 # # fig.tight_layout()
 # # fig.savefig(fname=f'subset_images/images_indian_ear.png', dpi=400)
 
+from typing import List, Dict
+from Concepts.CAV import CAV, get_or_train_cav
+from Concepts.helper import load_images_from_files, get_gradients_of_images, get_grad_model, get_bottleneck_model, \
+    get_activations_of_images
+from Dash_helper import get_class_labels, read_image, get_random_activation, save_images
 import os
-import tarfile
+import tensorflow as tf
+import shutil
+import numpy as np
 
-tarfiles = os.listdir('data/ImageNet2')
-tarfiles = [os.path.join('data/ImageNet2', file) for file in tarfiles]
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # my GPU is too small to save enough images in its V-RAM to get the gradients
 
-for file in tarfiles:
-    # Open the .tar file
-    tar = tarfile.open(file, "r")
+bottleneck = 'mixed8'
 
-    # Extract all the contents of the .tar file to a directory
-    tar.extractall(path=f'{file.rstrip(".tar")}/')
+cav1 = CAV.load_cav('./ACE_output/ImageNet/cavs/mixed8-toucan__userDefined_001.pkl')
+cav2 = CAV.load_cav('./ACE_output/ImageNet/cavs/mixed8-toucan__concept1.pkl')
 
-    # Close the .tar file
-    tar.close()
+class_folder = '../../data/ImageNet/n01843383'
 
-    os.remove(file)
+class_dct = get_class_labels(data_folder_path='../../data/ImageNet')
+img_filenames = [os.path.join(class_folder, filename) for filename in os.listdir(class_folder)]
+imgs = load_images_from_files(filenames=img_filenames, num_workers=1)
+
+cavs = [CAV.load_cav(f'./ACE_output/ImageNet/cavs_temp/mixed8-toucan__concept6-random500_{i}.pkl') for i in range(20)]
+
+model = tf.keras.applications.inception_v3.InceptionV3()
+bn_model = get_grad_model(model, bottleneck)
+grads = get_gradients_of_images(imgs, bn_model, class_dct['toucan'])
+tcav_scores = [cav.compute_tcav_score(grads) for cav in cavs]
+print(tcav_scores)
+
+# USER DEFINED
+filenames = [os.path.join('ACE_output/ImageNet/concepts/toucan/mixed8/toucan__concept6', file) for file in os.listdir('ACE_output/ImageNet/concepts/toucan/mixed8/toucan__concept6')]
+images = load_images_from_files(filenames)
+def create_new_concept(images, session_dir: str, bottleneck: str, model: tf.keras.models.Model):
+
+    concept_name = 'toucan_test'
+    concept_dir = os.path.join(session_dir, 'concepts', concept_name)
+
+    # create directory for storing the images of the user defined concept
+    if not os.path.exists(concept_dir):
+        os.makedirs(concept_dir)
+    else:
+        shutil.rmtree(concept_dir)
+    os.makedirs(os.path.join(concept_dir, 'images'))
+
+    # get activations of the random counterpart
+    random_activations = get_random_activation(session_dir, bottleneck)
+
+    # get activations of the images
+    concept_activations = get_activations_of_images(images, get_bottleneck_model(model, bottleneck))
+
+    # for each random counterpart create a CAV
+    cavs = []
+    for rnd_act in random_activations:
+        act_dct = {concept_name: concept_activations.reshape((concept_activations.shape[0], -1)),
+                   'random_counterpart': rnd_act.reshape((rnd_act.shape[0], -1))}
+        cavs.append(get_or_train_cav([concept_name, 'random_counterpart'], bottleneck,
+                                     os.path.join(session_dir, 'cavs'), act_dct, save=False, ow=True))
+    tcav_scores = [cav.compute_tcav_score(grads) for cav in cavs]
+
+
+    return tcav_scores
+
+scores = create_new_concept(images, './ACE_output/ImageNet', 'mixed8', model)
+print(scores)
+
+#TODO error in loading images